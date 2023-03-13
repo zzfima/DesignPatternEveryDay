d8cebf8f6d32c6e754fe9bb0747267134239fceb
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-03-13 12:31:22 +0200

    use Lazy generic class

--- CreationalDesignPatterns/Singletone/SingletoneThreadSafe/Client/Program.cs
+++ CreationalDesignPatterns/Singletone/SingletoneThreadSafe/Client/Program.cs
@@ -1,41 +1,51 @@
 ﻿using SingletoneThreadSafe.Models;
 
 Parallel.Invoke(new Action[]{
-    new Action(() =>Singletone.GetInstance.PrintDetails("Hello1")),
-    new Action(() =>Singletone.GetInstance.PrintDetails("Hello2")),
-    new Action(() =>Singletone.GetInstance.PrintDetails("Hello3")),
-    new Action(() =>Singletone.GetInstance.PrintDetails("Hello4")),
-    new Action(() =>Singletone.GetInstance.PrintDetails("Hello5")),
-    new Action(() =>Singletone.GetInstance.PrintDetails("Hello6")),});
+    new Action(()=>Singletone.GetInstance.PrintDetails("Hello1")),
+    new Action(()=>Singletone.GetInstance.PrintDetails("Hello2")),
+    new Action(()=>Singletone.GetInstance.PrintDetails("Hello3")),
+    new Action(()=>Singletone.GetInstance.PrintDetails("Hello4")),
+    new Action(()=>Singletone.GetInstance.PrintDetails("Hello5")),
+    new Action(()=>Singletone.GetInstance.PrintDetails("Hello6")),});
 
 Console.WriteLine("**************************");
 
 Parallel.Invoke(new Action[]{
-    new Action(() =>SingletoneTS.GetInstance.PrintDetails("World1")),
-    new Action(() =>SingletoneTS.GetInstance.PrintDetails("World2")),
-    new Action(() =>SingletoneTS.GetInstance.PrintDetails("World3")),
-    new Action(() =>SingletoneTS.GetInstance.PrintDetails("World4")),
-    new Action(() =>SingletoneTS.GetInstance.PrintDetails("World5")),
-    new Action(() =>SingletoneTS.GetInstance.PrintDetails("World6")),});
+    new Action(()=>SingletoneTS.GetInstance.PrintDetails("World1")),
+    new Action(()=>SingletoneTS.GetInstance.PrintDetails("World2")),
+    new Action(()=>SingletoneTS.GetInstance.PrintDetails("World3")),
+    new Action(()=>SingletoneTS.GetInstance.PrintDetails("World4")),
+    new Action(()=>SingletoneTS.GetInstance.PrintDetails("World5")),
+    new Action(()=>SingletoneTS.GetInstance.PrintDetails("World6")),});
 
 Console.WriteLine("**************************");
 
 Parallel.Invoke(new Action[]{
-    new Action(() =>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace1")),
-    new Action(() =>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace2")),
-    new Action(() =>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace3")),
-    new Action(() =>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace4")),
-    new Action(() =>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace5")),
-    new Action(() =>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace6")),});
+    new Action(()=>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace1")),
+    new Action(()=>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace2")),
+    new Action(()=>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace3")),
+    new Action(()=>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace4")),
+    new Action(()=>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace5")),
+    new Action(()=>SingletoneTSDoubleCheck.GetInstance.PrintDetails("Peace6")),});
 
 Console.WriteLine("**************************");
 
 Parallel.Invoke(new Action[]{
-    new Action(() =>SingletoneEager.GetInstance.PrintDetails("Land1")),
-    new Action(() =>SingletoneEager.GetInstance.PrintDetails("Land2")),
-    new Action(() =>SingletoneEager.GetInstance.PrintDetails("Land3")),
-    new Action(() =>SingletoneEager.GetInstance.PrintDetails("Land4")),
-    new Action(() =>SingletoneEager.GetInstance.PrintDetails("Land5")),
-    new Action(() =>SingletoneEager.GetInstance.PrintDetails("Land6")),});
+    new Action(()=>SingletoneEager.GetInstance.PrintDetails("Land1")),
+    new Action(()=>SingletoneEager.GetInstance.PrintDetails("Land2")),
+    new Action(()=>SingletoneEager.GetInstance.PrintDetails("Land3")),
+    new Action(()=>SingletoneEager.GetInstance.PrintDetails("Land4")),
+    new Action(()=>SingletoneEager.GetInstance.PrintDetails("Land5")),
+    new Action(()=>SingletoneEager.GetInstance.PrintDetails("Land6")),});
+
+Console.WriteLine("**************************");
+
+Parallel.Invoke(new Action[]{
+    new Action(()=>SingletoneLazy.GetInstance.PrintDetails("Air1")),
+    new Action(()=>SingletoneLazy.GetInstance.PrintDetails("Air2")),
+    new Action(()=>SingletoneLazy.GetInstance.PrintDetails("Air3")),
+    new Action(()=>SingletoneLazy.GetInstance.PrintDetails("Air4")),
+    new Action(()=>SingletoneLazy.GetInstance.PrintDetails("Air5")),
+    new Action(()=>SingletoneLazy.GetInstance.PrintDetails("Air6")),});
 
 Console.ReadLine();
---
+++ CreationalDesignPatterns/Singletone/SingletoneThreadSafe/Models/SingletoneLazy.cs
@@ -0,0 +1,24 @@
+﻿namespace SingletoneThreadSafe.Models
+{
+    internal sealed class SingletoneLazy
+    {
+        private static int counter = 0;
+        private static Lazy<SingletoneLazy> instance = new Lazy<SingletoneLazy>(() => new SingletoneLazy());
+
+        public static SingletoneLazy GetInstance
+        {
+            get => instance.Value;
+        }
+
+        private SingletoneLazy()
+        {
+            counter++;
+            Console.WriteLine("Counter Value Lazy" + counter.ToString());
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine($"[[[ {message} ]]]");
+        }
+    }
+}
